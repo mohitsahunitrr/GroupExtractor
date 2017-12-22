7c0cbe73859495224e55b8f51b7693e041f4637c
Mukul Hase <mukulhase@gmail.com>
2017-12-22 18:31:25 +0530

    Unicode Fix

--- sample.py
+++ sample.py
@@ -9,16 +9,17 @@ print('Exporting...')
 chats = driv.get_all_chats()
 ##Filter Group chats
 groupchats = filter(lambda chat:(type(chat) == GroupChat), chats)
-##Search Group chat for first occurence of group name given
+def convertStr(text):
+	return str(text.encode('utf-8').decode('ascii', 'ignore')) if text else "(empty)"
 for chat in groupchats:
 	##Create CSV
-	safe_name = str(chat.name.encode('utf-8').decode('ascii', 'ignore'))
+	safe_name = convertStr(chat.name)
 	ofile  = open(str(safe_name)+'.csv', "wb")
 	writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 	writer.writerow(['Name', 'Profile Name', 'Phone Number'])
 	##Iterate Through Group Participants
 	for participant in chat.get_participants():
-	    writer.writerow([participant.name, participant.push_name, participant.id])
+	    writer.writerow([convertStr(participant.name), convertStr(participant.push_name), participant.id])
 	ofile.close()
 	print("Done with " + safe_name)
 print("Done")
