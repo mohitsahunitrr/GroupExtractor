62d8d51c8cf1712eb722bab52ab95180763596c1
Mukul Hase <mukulhase@gmail.com>
2017-12-22 18:50:28 +0530

    Try Except

--- sample.py
+++ sample.py
@@ -12,14 +12,17 @@ groupchats = filter(lambda chat:(type(chat) == GroupChat), chats)
 def convertStr(text):
 	return str(text.encode('utf-8').decode('ascii', 'ignore')) if text else "(empty)"
 for chat in groupchats:
-	##Create CSV
-	safe_name = convertStr(chat.name)
-	ofile  = open(str(safe_name)+'.csv', "wb")
-	writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-	writer.writerow(['Name', 'Profile Name', 'Phone Number'])
-	##Iterate Through Group Participants
-	for participant in chat.get_participants():
-	    writer.writerow([convertStr(participant.name), convertStr(participant.push_name), participant.id])
-	ofile.close()
-	print("Done with " + safe_name)
+	try:
+		##Create CSV
+		safe_name = convertStr(chat.name)
+		ofile  = open(str(safe_name)+'.csv', "wb")
+		writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+		writer.writerow(['Name', 'Profile Name', 'Phone Number'])
+		##Iterate Through Group Participants
+		for participant in chat.get_participants():
+			writer.writerow([convertStr(participant.name), convertStr(participant.push_name), participant.id])
+		ofile.close()
+		print("Done with " + safe_name)
+	except:
+		pass
 print("Done")
