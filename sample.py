a6037a5238e053bbbf23b8dfefb3a2daec6218fe
Mukul Hase <mukulhase@gmail.com>
2017-12-22 17:10:48 +0530

    All groups export

--- sample.py
+++ sample.py
@@ -4,19 +4,21 @@ from webwhatsapi import GroupChat
 print("Scan QR")
 driv = webwhatsapi.WhatsAPIDriver()
 ##Scan QR Now
-name = raw_input("Enter Name of Group: ")
+print('Exporting...')
 ##Get all chats
 chats = driv.get_all_chats()
 ##Filter Group chats
 groupchats = filter(lambda chat:(type(chat) == GroupChat), chats)
 ##Search Group chat for first occurence of group name given
-chat = next(x for x in groupchats if x.name == name)
-##Create CSV
-ofile  = open('output.csv', "wb")
-writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-writer.writerow(['Name', 'Profile Name', 'Phone Number'])
-##Iterate Through Group Participants
-for participant in chat.get_participants():
-    writer.writerow([participant.name, participant.push_name, participant.id])
-ofile.close()
+for chat in groupchats:
+	##Create CSV
+	safe_name = str(chat.name.encode('utf-8').decode('ascii', 'ignore'))
+	ofile  = open(str(safe_name)+'.csv', "wb")
+	writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+	writer.writerow(['Name', 'Profile Name', 'Phone Number'])
+	##Iterate Through Group Participants
+	for participant in chat.get_participants():
+	    writer.writerow([participant.name, participant.push_name, participant.id])
+	ofile.close()
+	print("Done with " + safe_name)
 print("Done")
